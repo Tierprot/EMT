ebab1d439e04d86b59ef19d52b815c1c7ca3212e
Seva <sevastyan@rabdano.ru>
2019-03-02 15:41:59 +0300

    blah

--- check_gene_pairs.py
+++ check_gene_pairs.py
@@ -1,13 +1,18 @@
 from find_interactions import *
 import json
 from pprint import pprint
+from handythread import *
 
 gene_pairs_json_file = "gene_pairs.json"
 
 with open(gene_pairs_json_file) as f:    
     gene_pairs_json = json.load(f)
 
-for article in gene_pairs_json.values():
+N = len(gene_pairs_json.keys())
+counter = 0
+
+def process_article(article):
+    global counter,N
     gene_pairs = []
     # pairs = article["genes"].values()
     for pair in article["genes"].values():
@@ -15,6 +20,12 @@ for article in gene_pairs_json.values():
     check = check_interactions(gene_pairs)
     for pair,c in zip(article["genes"].values(),check):
         pair["database"] = c
+    counter += 1
+    print("%d/%d" % (counter,N))
+
+
+entries = gene_pairs_json.values()
+foreach(process_article,entries,threads=16)
 
 with open("gene_pairs_checked.json", "w") as f:
     json.dump(gene_pairs_json, f, indent = "\t", separators=(", ", " : "))
