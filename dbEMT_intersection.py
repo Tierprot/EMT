02d4576b5a7ddbd11012d6a34984bb5bfa00a3df
Seva <sevastyan@rabdano.ru>
2019-03-02 23:21:58 +0300

    dbEMT - показать что она вошла, cleanup

--- dbEMT_intersection.py
+++ dbEMT_intersection.py
@@ -1,23 +1,5 @@
 import json
 
-def flatten_json(y):
-    out = {}
-
-    def flatten(x, name=''):
-        if type(x) is dict:
-            for a in x:
-                flatten(x[a], name + a + '_')
-        elif type(x) is list:
-            i = 0
-            for a in x:
-                flatten(a, name + str(i) + '_')
-                i += 1
-        else:
-            out[name[:-1]] = x
-
-    flatten(y)
-    return out
-
 # load dbEMT
 path_to_dbEMT = "interaction_databases/dbEMT/emt.hsa.info_seq.txt"
 dbEMT = []
