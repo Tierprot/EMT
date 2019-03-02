63471094c02973261179555c29c8454714eb2d7f
Seva <sevastyan@rabdano.ru>
2019-03-02 23:22:37 +0300

    Check gene pairs 10

--- check_gene_pairs.py
+++ check_gene_pairs.py
@@ -4,13 +4,11 @@ from pprint import pprint
 from handythread import *
 from multiprocessing import Pool
 
-gene_pairs_json_file = "gene_pairs_test.json"
+gene_pairs_json_file = "gene_pairs.json"
 
 bases = {"BIOGRID" : "interaction_databases/BIOGRID/BIOGRID-ORGANISM-Homo_sapiens-3.5.170.tab2.txt",
          "REACTOME" : "interaction_databases/Reactome/Reactome.txt",
          "RNA" : "interaction_databases/RNA/RNA_all_reduced.txt"}
-
-# bases = {"REACTOME" : "interaction_databases/Reactome/Reactome.txt"}
 
 handlers = [open(x, "r") for x in bases.values()]
 files = [x.readlines() for x in handlers]
@@ -19,11 +17,9 @@ for f in handlers: f.close()
 with open(gene_pairs_json_file) as f:    
     gene_pairs_json = json.load(f)
 
-N = len(gene_pairs_json.keys())
-counter = 0
-
-def process_article(article):
+def process_article(entry):
     global counter,N,bases,files
+    pmid,article = entry
     gene_pairs = []
     # pairs = article["genes"].values()
     for pair in article["genes"].values():
@@ -31,15 +27,19 @@ def process_article(article):
     check = check_interactions(gene_pairs,bases,files)
     for pair,c in zip(article["genes"].values(),check):
         pair["database"] = c
-    counter += 1
-    print("%d/%d" % (counter,N), check)
-    # print(check)
+    print(pmid, check)
+    return((pmid, article))
 
+#entries = gene_pairs_json.values()
+#foreach(process_article,entries,threads=2)
+pool = Pool(16)
+res = pool.map(process_article, zip(gene_pairs_json.keys(),gene_pairs_json.values()))
 
-entries = gene_pairs_json.values()
-foreach(process_article,entries,threads=2)
-# pool = Pool(16)
-# pool.map(process_article, gene_pairs_json.values())
+# print(res)
+
+out = {}
+for pmid,article in res:
+   out[pmid] = article
 
 with open("gene_pairs_checked.json", "w") as f:
-    json.dump(gene_pairs_json, f, indent = "\t", separators=(", ", " : "))
+    json.dump(out, f, indent = "\t", separators=(", ", " : "))
