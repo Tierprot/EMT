714dec411936a0ccc3bed53aa64d3205e0cd159e
Tierprot <tierprot@gmail.com>
2019-03-03 12:21:20 +0300

    working template

--- DB_interface/app.py
+++ DB_interface/app.py
@@ -1,6 +1,6 @@
 from flask import Flask, render_template, request, redirect, url_for, jsonify
 from table_creation import make_table
-
+from mongoOperations import ask_db
 
 app = Flask(__name__)
 
@@ -31,8 +31,16 @@ partners = {
                 }
             }
 
+@app.route("/", methods=['GET'])
+def index():
+    return render_template('main.html')
 
-@app.route("/")
-def hello():
-    return render_template('main.html', test_data = make_table(partners))
 
+@app.route("/search_info", methods=['POST'])
+def search_info():
+    print('im inside')
+    gene_id = request.get_json()['gene_id']
+    print(gene_id)
+    #final_json = ask_db(gene_id)
+    #return make_table(final_json)
+    return jsonify(html=make_table(partners))
